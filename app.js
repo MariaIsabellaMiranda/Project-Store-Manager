0a0b28964af84c5ad62fde8584f00fb0971390a5
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-11 18:49:15 -0300

    fix: correções para funcionalidade do projeto e correções de lint

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const productsRouter = require('./routers/productsRouter');
 
 const app = express();
 app.use(express.json());
-app.use("/products", productsRouter);
-app.use("/sales", productsRouter);
+app.use('/products', productsRouter);
+app.use('/sales', productsRouter);
+
+// app.use(rescue());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
