10f1fda7256d5e43bc5101362365c6f86dfad9dd
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-15 00:30:35 -0300

    feat: Criado dois módulos na camada models, um que realiza uma query cadastrando uma venda na tabela sales e outro que realiza uma query para cadastrar os ids de produtos, vendas e quantidades na tabela sales_products

---
+++ models/salesModels.js
@@ -0,0 +1,22 @@
+const connection = require('./connection');
+
+const createIdSales = async () => {
+  const [sales] = await connection.execute(
+    'INSERT INTO StoreManager.sales (date) VALUES (NOW());',
+  );
+
+  return { id: sales.insertId };
+};
+
+const createSales = async (salesProducts) => {
+  const { id, productId, quantity } = salesProducts;
+  
+  await connection.execute(
+    `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+    VALUES (?, ?, ?);`, [id, productId, quantity],
+  );
+
+  return { productId, quantity };
+};
+
+module.exports = { createIdSales, createSales };
