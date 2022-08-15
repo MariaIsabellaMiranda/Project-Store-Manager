38d9e02db9fcd4a268ee5a332fb2e2fc982c48f1
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-15 00:24:45 -0300

    feat: Criado módulo de controller que retorna para o cliente o status e um array com os produtos e vendas cadastradas ou um código e message de erro caso não passe na validação

---
+++ controllers/salesControllers.js
@@ -0,0 +1,13 @@
+const salesServices = require('../services/salesServices');
+
+const createSales = async (req, res) => {
+  const productsSales = req.body;
+
+  const { code, data, message } = await salesServices.createSales(productsSales);
+
+  if (!data) return res.status(code).json(message);
+
+  res.status(code).json(data);
+};
+
+module.exports = { createSales };
