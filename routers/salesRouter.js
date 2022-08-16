8d6f0f8d325a68b5434b55ba7f0a93de9f775437
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-16 18:18:07 -0300

    feat: Criadas rotas para requisições que listam todas as vendas e que pega as informações da venda conforme id informado

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const salesControllers = require('../controllers/salesControllers');
 
 router.post('/', salesControllers.createSales);
-
-router.get('/:id', () => {});
+router.get('/', salesControllers.getAllSales);
+router.get('/:id', salesControllers.getSalesId);
 
 module.exports = router;
