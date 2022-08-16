3e09f66b02edc9cd3dc21bc56237c0fe4e8c78b6
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-15 21:05:53 -0300

    feat: Criado módulo na camda services que faz validações para cadastrar uma nova venda e solicita o cadastramento para a camada models, além de retornar para a camada de controller os status, menssagens ou produto cadastrado de acordo com sucesso ou falhas

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,20 +1,23 @@
 const salesModels = require('../models/salesModels');
+const salesValidation = require('../utils/salesValidation');
 
 const createSales = async (productSales) => {
+  const salesError = salesValidation.salesValidSchema(productSales);
+
+  if (salesError) return { code: Number(salesError.code), message: salesError.message };
+
+  const checkProductId = await salesValidation.checkProductId(productSales);
+
+  if (checkProductId.includes(false)) return { code: 404, message: 'Product not found' };
+
   const { id } = await salesModels.createIdSales();
 
   const sales = await Promise.all(
     productSales.map(({ productId, quantity }) =>
     salesModels.createSales({ id, productId, quantity })),
     );
-    
-  return {
-    code: 201,
-    data: {
-      id,
-      itemsSold: sales,
-    },
-  };
+
+  return { code: 201, data: { id, itemsSold: sales } };
 };
 
 module.exports = { createSales };
