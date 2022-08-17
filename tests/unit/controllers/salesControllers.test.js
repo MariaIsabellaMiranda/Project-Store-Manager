e35a79e3aaacad77923925d328c5fbb27d04d354
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-17 00:43:48 -0300

    test: Implementados testes do controller onde são listadas todas as vendas

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -9,6 +9,11 @@ const {
   SALES_PRODUCTID_INVALID,
   ERR_SALES_PRODUCTID_INVALID,
   CODE_400,
+  DATA_GET_SALES_ALL,
+  DATA_GET_SALES_ID,
+  ERR_SALES_ALL,
+  CODE_404,
+  CODE_200,
 } = require("../utils/constants");
 
 describe('Testa as funcionalidades do controller que cadastra novas vendas', () => {
@@ -72,6 +77,65 @@ describe('Testa as funcionalidades do controller que cadastra novas vendas', () 
 
       expect(response.json.calledWith(ERR_SALES_PRODUCTID_INVALID))
       .to.be.equal(true);
+    });
+  });
+});
+
+describe("Testa as funcionalidades do controller que busca por todas as vendas e retorna os respectivos códigos e menssagens", () => {
+  describe("Testa quando há vendas cadastradas", () => {
+    const request = {};
+    const response = {};
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesServices, "getAllSales").resolves({
+        code: CODE_200,
+        data: DATA_GET_SALES_ALL,
+      });
+    });
+    after(() => {
+      salesServices.getAllSales.restore();
+    });
+
+    it("se retorna o status com o código 200", async () => {
+      await salesControllers.getAllSales(request, response);
+
+      expect(response.status.calledWith(CODE_200)).to.be.equal(true);
+    });
+
+    it("se retorna um json com os produtos cadastrados", async () => {
+      await salesControllers.getAllSales(request, response);
+
+      expect(response.json.calledWith(DATA_GET_SALES_ALL)).to.be.equal(true);
+    });
+  });
+
+  describe("Testa quando não há vendas cadastradas", () => {
+    const request = {};
+    const response = {};
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesServices, "getAllSales").resolves({
+        code: CODE_404,
+        data: [],
+      });
+    });
+    after(() => {
+      salesServices.getAllSales.restore();
+    });
+    it("se retorna um array vazio", async () => {
+      await salesControllers.getAllSales(request, response);
+
+      expect(response.json.calledWith([])).to.be.equal(true);
+    });
+
+    it("se retorna o status com o código 404", async () => {
+      await salesControllers.getAllSales(request, response);
+
+      expect(response.status.calledWith(CODE_404)).to.be.equal(true);
     });
   });
 });
