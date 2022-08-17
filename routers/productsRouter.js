acb1a1317478c3f485247e258cb26fef22229b6e
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-17 19:25:47 -0300

    fix: Trocado de lugar rota que faz busca do produto através do search para corrigir o código

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const rescue = require('../middlewares/rescue');
 const productsController = require('../controllers/productsControllers');
 
-router.delete('/search', rescue(productsController.getProductsSearch));
+router.get('/search', rescue(productsController.getProductsSearch));
                                 
 router.get('/', rescue(productsController.getAllProducts));
 
@@ -13,6 +13,5 @@ router.post('/', rescue(productsController.createProduct));
 router.put('/:id', rescue(productsController.updateProductById));
 
 router.delete('/:id', rescue(productsController.deleteProducts));
-
 
 module.exports = router;
