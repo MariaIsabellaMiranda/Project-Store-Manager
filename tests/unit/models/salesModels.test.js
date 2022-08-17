740d29eb5eef3ac8cddf05860bd504706ca29b07
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-16 23:58:53 -0300

    test: Impĺementado testes para camada models onde lista as vendas conforme Id passado por parâmetro

--- tests/unit/models/salesModels.test.js
+++ tests/unit/models/salesModels.test.js
@@ -2,7 +2,12 @@ const sinon = require("sinon");
 const { expect } = require("chai");
 const connection = require("../../../models/connection");
 const salesModels = require('../../../models/salesModels');
-const { SALES_PRODUCTS, SALES_ALL } = require("../utils/constants.js");
+const {
+  SALES_PRODUCTS,
+  GET_SALES_ALL,
+  GET_SALES_ID,
+  ID,
+} = require("../utils/constants.js");
 
 describe('Testa as funcionalidades dos módulos que cadastram novas vendas', () => {
   describe('Testa quando é possível cadastrar uma venda na tabela sales que retorna um Id da venda', () => {
@@ -54,7 +59,7 @@ describe('Testa as funcionalidades dos módulos que cadastram novas vendas', () 
 describe("Testa as funcionalidades dos módulos da camada models que listam todas as vendas", () => {
   describe("Testa quando a busca pela lista completa é bem sucedida", () => {
     before(async () => {
-      const result = SALES_ALL;
+      const result = GET_SALES_ALL;
       sinon.stub(connection, "execute").resolves(result);
     });
     after(async () => {
@@ -99,4 +104,52 @@ describe("Testa as funcionalidades dos módulos da camada models que listam toda
       expect(result).to.be.empty;
     });
   });
+
+  describe('Testa as funcionalidades do módulo da camada models que busca vendas pelo Id', () => {
+  describe('Testa quando a busca no DB através do id é bem sucedida', () => {
+    before(async () => {
+      const result = [GET_SALES_ID];
+      sinon.stub(connection, 'execute').resolves(result);
+    });
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it("se retorna um array", async () => {
+      const result = await salesModels.getSalesId(ID);
+      expect(result).to.be.a("array");
+    });
+
+    it("se o array possui um objeto dentro", async () => {
+      const result = await salesModels.getSalesId(ID);
+      expect(result[0]).to.be.a("object");
+    });
+
+    it('se os objetos tem as propriedades: "date", "productId" e "quantity"', async () => {
+      const result = await salesModels.getSalesId(ID);
+      const item = result[0];
+      expect(item).to.include.all.keys("date", "productId", "quantity");
+    });
+  });
+
+  describe('Testa quando o id passado não corresponde a nenhum produto', () => {
+    before(async () => {
+      const result = [[], []];
+      sinon.stub(connection, "execute").resolves(result);
+    });
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it("se retorna um array", async () => {
+      const result = await salesModels.getSalesId();
+      expect(result).to.be.a("array");
+    });
+
+    it("se o array está vazio", async () => {
+      const result = await salesModels.getSalesId();
+      expect(result).to.be.empty;
+    });
+  });
+});
 });
